3aaa373b9321a4343fcdb77577a4c6a928489552
Kevin Lawrie <climbvertigo@gmail.com>
2014-08-22 15:28:44 +0200

    Header/Apply/Login

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.set('views', __dirname + '/views');
 
 // Swig will cache templates for you, but you can disable
 // that and use Express's caching instead, if you like:
-app.set('view cache', true);
+app.set('view cache', false);
 // To disable Swig's cache, do the following:
 swig.setDefaults({ cache: false });
 // NOTE: You should always cache templates in a production environment.
